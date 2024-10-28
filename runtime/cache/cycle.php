61fd3ece01e82e779635cb6ac00a647480ec59ca
kaveh1999nazari <kaveh1999nazari@gmail.com>
2024-10-28 14:04:51 +0330

    .

---
+++ runtime/cache/cycle.php
@@ -0,0 +1,41 @@
+<?php return array (
+  'user' => 
+  array (
+    1 => 'App\\Entity\\User',
+    2 => 'Cycle\\ORM\\Mapper\\Mapper',
+    3 => 'Cycle\\ORM\\Select\\Source',
+    4 => 'Cycle\\ORM\\RepositoryInterface',
+    5 => 'default',
+    6 => 'users',
+    7 => 
+    array (
+      0 => 'id',
+    ),
+    8 => 
+    array (
+      0 => 'id',
+    ),
+    9 => 
+    array (
+      'id' => 'id',
+      'mobile' => 'mobile',
+      'password' => 'password',
+    ),
+    10 => 
+    array (
+    ),
+    12 => NULL,
+    13 => 
+    array (
+      'id' => 'int',
+    ),
+    14 => 
+    array (
+    ),
+    19 => NULL,
+    20 => 
+    array (
+      'id' => 2,
+    ),
+  ),
+);
