e2b537b7768764463ee02334e8fecf18e40a6dcc
kaveh1999nazari <kaveh1999nazari@gmail.com>
2024-10-29 09:34:03 +0330

    clean the code

--- app/src/Attribute/ValidateBy.php
+++ app/src/Attribute/ValidateBy.php
@@ -8,9 +8,5 @@ use Doctrine\Common\Annotations\Annotation\NamedArgumentConstructor;
 #[Attribute(Attribute::TARGET_METHOD), NamedArgumentConstructor]
 class ValidateBy
 {
-    public function __construct(
-        public readonly string $validatorClass
-    )
-    {
-    }
+    public function __construct(public readonly string $validatorClass){}
 }
