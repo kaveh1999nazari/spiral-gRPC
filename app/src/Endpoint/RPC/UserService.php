97f80a08ac2d42474d8fd8a7bb6162bf740d8682
kaveh1999nazari <kaveh1999nazari@gmail.com>
2024-10-30 08:00:54 +0330

    add Login and use ORMInterface instead of UserRepository

--- app/src/Endpoint/RPC/UserService.php
+++ app/src/Endpoint/RPC/UserService.php
@@ -3,17 +3,27 @@
 namespace App\Endpoint\RPC;
 
 use App\Attribute\ValidateBy;
-use App\Repository\UserRepository;
+use App\Entity\User;
 use App\Request\UserRegisterRequest;
+use Cycle\ORM\ORMInterface;
+use Firebase\JWT\JWT;
+use GRPC\User\LoginReq;
+use GRPC\User\LoginRes;
 use GRPC\User\RegisterReq;
 use GRPC\User\UserGrpcInterface;
+use Spiral\Auth\AuthContextInterface;
+use Spiral\Auth\TokenInterface;
+use Spiral\Auth\TokenStorageInterface;
 use Spiral\RoadRunner\GRPC;
 use GRPC\User\RegisterRes;
 
 class UserService implements UserGrpcInterface
 {
+    private $secrectKey = "your_key";
     public function __construct(
-        protected readonly UserRepository $userRepository
+        protected readonly ORMInterface $orm,
+        private readonly AuthContextInterface $auth,
+        private readonly TokenStorageInterface $tokens
     ){}
 
     #[ValidateBy(UserRegisterRequest::class)]
@@ -22,10 +32,37 @@ class UserService implements UserGrpcInterface
         $mobile = $in->getMobile();
         $password = password_hash($in->getPassword(), PASSWORD_BCRYPT);
 
-        $user = $this->userRepository->create($mobile, $password);
+        $user = $this->orm->getRepository(User::class)
+            ->create($mobile, $password);
 
         $res = new RegisterRes();
         $res->setId($user->getId());
         return $res;
+    }
+
+    public function Login(GRPC\ContextInterface $ctx, LoginReq $in): LoginRes
+    {
+        $mobile = $in->getMobile();
+        $password = $in->getPassword();
+
+        print_r($mobile);
+
+        $user = $this->orm->getRepository(User::class)
+            ->findByMobile($mobile);
+
+        if ($user && password_verify($password, $user->getPassword()))
+        {
+            $token = $this->tokens->create(['sub' => $user->getId()]);
+
+            print_r($token->getToken());
+
+            $response = new LoginRes();
+            $response->setToken($token->getID());
+            return $response;
+        }
+        $response = new LoginRes();
+        $response->setMessage("Authentication failed.");
+        return $response;
+
     }
 }
