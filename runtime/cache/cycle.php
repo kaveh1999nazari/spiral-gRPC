c5784c7c04c6b2ffabf63b279a9f4e36defb2530
kaveh1999nazari <kaveh1999nazari@gmail.com>
2024-10-30 07:54:07 +0330

    change RepositoryInterface to UserRepository

--- runtime/cache/cycle.php
+++ runtime/cache/cycle.php
@@ -4,7 +4,7 @@
     1 => 'App\\Entity\\User',
     2 => 'Cycle\\ORM\\Mapper\\Mapper',
     3 => 'Cycle\\ORM\\Select\\Source',
-    4 => 'Cycle\\ORM\\RepositoryInterface',
+    4 => 'App\\Repository\\UserRepository',
     5 => 'default',
     6 => 'users',
     7 => 
@@ -28,6 +28,48 @@
     13 => 
     array (
       'id' => 'int',
+    ),
+    14 => 
+    array (
+    ),
+    19 => NULL,
+    20 => 
+    array (
+      'id' => 2,
+    ),
+  ),
+  'token' => 
+  array (
+    1 => 'Spiral\\Cycle\\Auth\\Token',
+    2 => 'Cycle\\ORM\\Mapper\\Mapper',
+    3 => 'Cycle\\ORM\\Select\\Source',
+    4 => 'Cycle\\ORM\\Select\\Repository',
+    5 => 'default',
+    6 => 'auth_tokens',
+    7 => 
+    array (
+      0 => 'id',
+    ),
+    8 => 
+    array (
+      0 => 'id',
+    ),
+    9 => 
+    array (
+      'id' => 'id',
+      'hashedValue' => 'hashed_value',
+      'createdAt' => 'created_at',
+      'expiresAt' => 'expires_at',
+      'payload' => 'payload',
+    ),
+    10 => 
+    array (
+    ),
+    12 => NULL,
+    13 => 
+    array (
+      'createdAt' => 'datetime',
+      'expiresAt' => 'datetime',
     ),
     14 => 
     array (
