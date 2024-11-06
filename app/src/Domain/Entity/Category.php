c0422f4d64e82b87cc54108a5096a314a91e4d0a
kaveh1999nazari <kaveh1999nazari@gmail.com>
2024-11-06 07:49:48 +0330

    add category Entity

---
+++ app/src/Domain/Entity/Category.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Domain\Entity;
+
+use App\Domain\Repository\CategoryRepository;
+use Cycle\Annotated\Annotation\Entity;
+use Cycle\Annotated\Annotation\Column;
+
+#[Entity(repository: CategoryRepository::class, table: "categories")]
+class Category
+{
+    #[Column(type:"primary")]
+    private int $id;
+    #[Column(type:"string")]
+    private string $name;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    // Getter and Setter for Name
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+}
