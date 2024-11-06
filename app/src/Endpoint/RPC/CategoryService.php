365153ac4e7cc30094e69351c990c50ed68d9202
kaveh1999nazari <kaveh1999nazari@gmail.com>
2024-11-06 07:50:02 +0330

    add category Service

---
+++ app/src/Endpoint/RPC/CategoryService.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Endpoint\RPC;
+
+use App\Domain\Attribute\AuthenticatedBy;
+use App\Domain\Entity\Category;
+use Cycle\ORM\ORMInterface;
+use GRPC\Admin\AdminGrpcInterface;
+use GRPC\Admin\categoryCreateRequest;
+use GRPC\Admin\categoryCreateResponse;
+use Spiral\RoadRunner\GRPC;
+
+
+class CategoryService implements AdminGrpcInterface
+{
+    public function __construct(protected readonly ORMInterface $orm){}
+    #[AuthenticatedBy]
+    public function categoryCreate(GRPC\ContextInterface $ctx, CategoryCreateRequest $in): CategoryCreateResponse
+    {
+        $name = $in->getName();
+
+        //$category = $this->orm->getRepository(Category::class)->create($name);
+
+        $response = new categoryCreateResponse();
+        //$response->setId($category->getId());
+        $response->setId("12");
+        return $response;
+    }
+
+}
