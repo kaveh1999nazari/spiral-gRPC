d26780aa500a41baa7d242ba297c445fbb1956b6
kaveh1999nazari <kaveh1999nazari@gmail.com>
2024-11-06 07:49:56 +0330

    add category Repository

---
+++ app/src/Domain/Repository/CategoryRepository.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Domain\Repository;
+
+use App\Domain\Entity\Category;
+use Cycle\ORM\EntityManager;
+use Cycle\ORM\Select;
+use Cycle\ORM\Select\Repository;
+
+class CategoryRepository extends Repository
+{
+
+    public function __construct(Select $select, private readonly EntityManager $entityManager)
+    {
+        parent::__construct($select);
+    }
+
+    public function create(string $name): Category
+    {
+        $category = new Category();
+        $category->setName($name);
+
+        $this->entityManager->persist($category);
+        $this->entityManager->run();
+
+        return $category;
+    }
+
+}
