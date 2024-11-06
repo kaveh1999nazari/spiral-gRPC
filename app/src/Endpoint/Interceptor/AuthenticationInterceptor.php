a152faa2fa6e35d708594e2c94b3b089712f4f4f
kaveh1999nazari <kaveh1999nazari@gmail.com>
2024-11-06 17:09:39 +0330

    .

--- app/src/Endpoint/Interceptor/AuthenticationInterceptor.php
+++ app/src/Endpoint/Interceptor/AuthenticationInterceptor.php
@@ -3,30 +3,33 @@
 namespace App\Endpoint\Interceptor;
 
 use App\Domain\Attribute\AuthenticatedBy;
+use Google\Rpc\Code;
 use Spiral\Interceptors\Context\CallContextInterface;
 use Spiral\Interceptors\HandlerInterface;
 use Spiral\Interceptors\InterceptorInterface;
-use Spiral\Auth\AuthContextInterface;
 use Spiral\Auth\Exception\AuthException;
+use Spiral\RoadRunner\GRPC\Exception\GRPCException;
+use Spiral\RoadRunner\GRPC\StatusCode;
 
 class AuthenticationInterceptor implements InterceptorInterface
 {
-    public function __construct(
-        private AuthContextInterface $authContext
-    ) {}
 
     public function intercept(CallContextInterface $context, HandlerInterface $handler): mixed
     {
-        print_r($this->authContext->getToken());
+        //print_r($context->getArguments()['ctx']);
+        $token = (string) $context->getArguments()['ctx']['authorization'][0];
+        if (empty($token)) {
+            throw new GRPCException(
+                //message: json_encode($token->getErrors()),
+                code: Code::UNAUTHENTICATED
+            );
+        }
+
+        print_r($context);
+
         $requestClass = $context->getTarget()->getPath();
         $reflectMethod = new \ReflectionMethod($context->getArguments()['service'], $requestClass[1]);
         $attributeDetails = $reflectMethod->getAttributes(AuthenticatedBy::class);
-
-        $token = $this->authContext->getToken();
-
-        if (!$token) {
-            throw new AuthException('Access denied: User not authenticated.');
-        }
 
         if ($attributeDetails) {
             $requiredRule = $attributeDetails[0]->getArguments()[0];
