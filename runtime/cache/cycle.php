ec688345905a0fc97fd10668276f8f17d4eea137
kaveh1999nazari <kaveh1999nazari@gmail.com>
2024-11-07 11:20:12 +0330

    .

--- runtime/cache/cycle.php
+++ runtime/cache/cycle.php
@@ -1,4 +1,42 @@
 <?php return array (
+  'category' => 
+  array (
+    1 => 'App\\Domain\\Entity\\Category',
+    2 => 'Cycle\\ORM\\Mapper\\Mapper',
+    3 => 'Cycle\\ORM\\Select\\Source',
+    4 => 'App\\Domain\\Repository\\CategoryRepository',
+    5 => 'default',
+    6 => 'categories',
+    7 => 
+    array (
+      0 => 'id',
+    ),
+    8 => 
+    array (
+      0 => 'id',
+    ),
+    9 => 
+    array (
+      'id' => 'id',
+      'name' => 'name',
+    ),
+    10 => 
+    array (
+    ),
+    12 => NULL,
+    13 => 
+    array (
+      'id' => 'int',
+    ),
+    14 => 
+    array (
+    ),
+    19 => NULL,
+    20 => 
+    array (
+      'id' => 2,
+    ),
+  ),
   'user' => 
   array (
     1 => 'App\\Entity\\User',
@@ -21,44 +59,6 @@
       'mobile' => 'mobile',
       'password' => 'password',
       'rule' => 'rule',
-    ),
-    10 => 
-    array (
-    ),
-    12 => NULL,
-    13 => 
-    array (
-      'id' => 'int',
-    ),
-    14 => 
-    array (
-    ),
-    19 => NULL,
-    20 => 
-    array (
-      'id' => 2,
-    ),
-  ),
-  'category' => 
-  array (
-    1 => 'App\\Domain\\Entity\\Category',
-    2 => 'Cycle\\ORM\\Mapper\\Mapper',
-    3 => 'Cycle\\ORM\\Select\\Source',
-    4 => 'App\\Domain\\Repository\\CategoryRepository',
-    5 => 'default',
-    6 => 'categories',
-    7 => 
-    array (
-      0 => 'id',
-    ),
-    8 => 
-    array (
-      0 => 'id',
-    ),
-    9 => 
-    array (
-      'id' => 'id',
-      'name' => 'name',
     ),
     10 => 
     array (
--- runtime/cache/listeners/fc30d4dfd70a6c5a9fd1d4c309e40210.php
+++ runtime/cache/listeners/fc30d4dfd70a6c5a9fd1d4c309e40210.php
@@ -1,5 +1,5 @@
 <?php return array (
-  0 => 'App\\Entity\\User',
-  1 => 'App\\Domain\\Entity\\Category',
+  0 => 'App\\Domain\\Entity\\Category',
+  1 => 'App\\Entity\\User',
   2 => 'Spiral\\Cycle\\Auth\\Token',
 );
