a3adc42abd48a9f01c4b91ae17f819d993c01ddc
kaveh1999nazari <kaveh1999nazari@gmail.com>
2024-11-10 09:53:08 +0330

    change to admin

--- app/src/Endpoint/RPC/CategoryService.php
+++ app/src/Endpoint/RPC/CategoryService.php
@@ -14,7 +14,7 @@ use Spiral\RoadRunner\GRPC;
 class CategoryService implements AdminGrpcInterface
 {
     public function __construct(protected readonly ORMInterface $orm){}
-    #[AuthenticatedBy(['guest', 'user2'])]
+    #[AuthenticatedBy(['admin'])]
     public function categoryCreate(GRPC\ContextInterface $ctx, CategoryCreateRequest $in): CategoryCreateResponse
     {
         $name = $in->getName();
