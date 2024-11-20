767f2d53c87d5e5eeb819107adf3e1264453e645
kaveh1999nazari <kaveh1999nazari@gmail.com>
2024-11-20 12:29:42 +0330

    .

--- runtime/cache/listeners/fc30d4dfd70a6c5a9fd1d4c309e40210.php
+++ runtime/cache/listeners/fc30d4dfd70a6c5a9fd1d4c309e40210.php
@@ -1,9 +1,10 @@
 <?php return array (
   0 => 'App\\Domain\\Entity\\ProductOption',
-  1 => 'App\\Domain\\Entity\\OptionValue',
-  2 => 'App\\Domain\\Entity\\Category',
-  3 => 'App\\Domain\\Entity\\User',
-  4 => 'App\\Domain\\Entity\\Product',
-  5 => 'App\\Domain\\Entity\\Option',
-  6 => 'Spiral\\Cycle\\Auth\\Token',
+  1 => 'App\\Domain\\Entity\\ProductPrice',
+  2 => 'App\\Domain\\Entity\\OptionValue',
+  3 => 'App\\Domain\\Entity\\Category',
+  4 => 'App\\Domain\\Entity\\User',
+  5 => 'App\\Domain\\Entity\\Product',
+  6 => 'App\\Domain\\Entity\\Option',
+  7 => 'Spiral\\Cycle\\Auth\\Token',
 );
