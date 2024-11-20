fc402ee39e407bb00839bd5ad0c7dec7b5b15568
kaveh1999nazari <kaveh1999nazari@gmail.com>
2024-11-20 10:52:08 +0330

    add option seeder

---
+++ app/src/database/seeder/optionSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\database\seeder;
+
+
+use App\Domain\Entity\Option;
+use Spiral\DatabaseSeeder\Attribute\Seeder;
+use Spiral\DatabaseSeeder\Seeder\AbstractSeeder;
+
+#[Seeder]
+class optionSeeder extends AbstractSeeder
+{
+    public function run(): \Generator
+    {
+        $option1 = new Option();
+        $option1->setName('color');
+        yield $option1;
+
+        $option2 = new Option();
+        $option2->setName('capacity');
+        yield $option2;
+
+        $option3 = new Option();
+        $option3->setName('size');
+        yield $option3;
+
+        $option4 = new Option();
+        $option4->setName('ram');
+        yield $option4;
+    }
+}
