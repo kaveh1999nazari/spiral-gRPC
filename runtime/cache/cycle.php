0c5f3c43899b0da24af2ea694d3cd44edc3a1ed7
kaveh1999nazari <kaveh1999nazari@gmail.com>
2024-11-23 14:05:46 +0330

    .

--- runtime/cache/cycle.php
+++ runtime/cache/cycle.php
@@ -1,4 +1,99 @@
 <?php return array (
+  'option' => 
+  array (
+    1 => 'App\\Domain\\Entity\\Option',
+    2 => 'Cycle\\ORM\\Mapper\\Mapper',
+    3 => 'Cycle\\ORM\\Select\\Source',
+    4 => 'Cycle\\ORM\\Select\\Repository',
+    5 => 'default',
+    6 => 'options',
+    7 => 
+    array (
+      0 => 'id',
+    ),
+    8 => 
+    array (
+      0 => 'id',
+    ),
+    9 => 
+    array (
+      'id' => 'id',
+      'name' => 'name',
+    ),
+    10 => 
+    array (
+    ),
+    12 => NULL,
+    13 => 
+    array (
+      'id' => 'int',
+    ),
+    14 => 
+    array (
+    ),
+    19 => NULL,
+    20 => 
+    array (
+      'id' => 2,
+    ),
+  ),
+  'productPrice' => 
+  array (
+    1 => 'App\\Domain\\Entity\\ProductPrice',
+    2 => 'Cycle\\ORM\\Mapper\\Mapper',
+    3 => 'Cycle\\ORM\\Select\\Source',
+    4 => 'App\\Domain\\Repository\\ProductPriceRepository',
+    5 => 'default',
+    6 => 'productprices',
+    7 => 
+    array (
+      0 => 'id',
+    ),
+    8 => 
+    array (
+      0 => 'id',
+    ),
+    9 => 
+    array (
+      'id' => 'id',
+      'options' => 'options',
+      'price' => 'price',
+      'product_id' => 'product_id',
+    ),
+    10 => 
+    array (
+      'product' => 
+      array (
+        0 => 12,
+        1 => 'product',
+        3 => 10,
+        2 => 
+        array (
+          30 => true,
+          31 => false,
+          33 => 'product_id',
+          32 => 
+          array (
+            0 => 'id',
+          ),
+        ),
+      ),
+    ),
+    12 => NULL,
+    13 => 
+    array (
+      'id' => 'int',
+      'product_id' => 'int',
+    ),
+    14 => 
+    array (
+    ),
+    19 => NULL,
+    20 => 
+    array (
+      'id' => 2,
+    ),
+  ),
   'productOption' => 
   array (
     1 => 'App\\Domain\\Entity\\ProductOption',
@@ -90,14 +185,14 @@
       'id' => 2,
     ),
   ),
-  'productPrice' => 
+  'user' => 
   array (
-    1 => 'App\\Domain\\Entity\\ProductPrice',
+    1 => 'App\\Domain\\Entity\\User',
     2 => 'Cycle\\ORM\\Mapper\\Mapper',
     3 => 'Cycle\\ORM\\Select\\Source',
-    4 => 'App\\Domain\\Repository\\ProductPriceRepository',
+    4 => 'App\\Domain\\Repository\\UserRepository',
     5 => 'default',
-    6 => 'productprices',
+    6 => 'users',
     7 => 
     array (
       0 => 'id',
@@ -109,90 +204,17 @@
     9 => 
     array (
       'id' => 'id',
-      'options' => 'options',
-      'price' => 'price',
-      'product_id' => 'product_id',
+      'mobile' => 'mobile',
+      'password' => 'password',
+      'roles' => 'roles',
     ),
     10 => 
     array (
-      'product' => 
-      array (
-        0 => 12,
-        1 => 'product',
-        3 => 10,
-        2 => 
-        array (
-          30 => true,
-          31 => false,
-          33 => 'product_id',
-          32 => 
-          array (
-            0 => 'id',
-          ),
-        ),
-      ),
     ),
     12 => NULL,
     13 => 
     array (
       'id' => 'int',
-      'product_id' => 'int',
-    ),
-    14 => 
-    array (
-    ),
-    19 => NULL,
-    20 => 
-    array (
-      'id' => 2,
-    ),
-  ),
-  'optionValue' => 
-  array (
-    1 => 'App\\Domain\\Entity\\OptionValue',
-    2 => 'Cycle\\ORM\\Mapper\\Mapper',
-    3 => 'Cycle\\ORM\\Select\\Source',
-    4 => 'Cycle\\ORM\\Select\\Repository',
-    5 => 'default',
-    6 => 'optionvalues',
-    7 => 
-    array (
-      0 => 'id',
-    ),
-    8 => 
-    array (
-      0 => 'id',
-    ),
-    9 => 
-    array (
-      'id' => 'id',
-      'name' => 'name',
-      'option_id' => 'option_id',
-    ),
-    10 => 
-    array (
-      'option' => 
-      array (
-        0 => 12,
-        1 => 'option',
-        3 => 10,
-        2 => 
-        array (
-          30 => true,
-          31 => false,
-          33 => 'option_id',
-          32 => 
-          array (
-            0 => 'id',
-          ),
-        ),
-      ),
-    ),
-    12 => NULL,
-    13 => 
-    array (
-      'id' => 'int',
-      'option_id' => 'int',
     ),
     14 => 
     array (
@@ -264,14 +286,14 @@
       'id' => 2,
     ),
   ),
-  'user' => 
+  'optionValue' => 
   array (
-    1 => 'App\\Domain\\Entity\\User',
+    1 => 'App\\Domain\\Entity\\OptionValue',
     2 => 'Cycle\\ORM\\Mapper\\Mapper',
     3 => 'Cycle\\ORM\\Select\\Source',
-    4 => 'App\\Domain\\Repository\\UserRepository',
+    4 => 'Cycle\\ORM\\Select\\Repository',
     5 => 'default',
-    6 => 'users',
+    6 => 'optionvalues',
     7 => 
     array (
       0 => 'id',
@@ -283,17 +305,33 @@
     9 => 
     array (
       'id' => 'id',
-      'mobile' => 'mobile',
-      'password' => 'password',
-      'roles' => 'roles',
+      'name' => 'name',
+      'option_id' => 'option_id',
     ),
     10 => 
     array (
+      'option' => 
+      array (
+        0 => 12,
+        1 => 'option',
+        3 => 10,
+        2 => 
+        array (
+          30 => true,
+          31 => false,
+          33 => 'option_id',
+          32 => 
+          array (
+            0 => 'id',
+          ),
+        ),
+      ),
     ),
     12 => NULL,
     13 => 
     array (
       'id' => 'int',
+      'option_id' => 'int',
     ),
     14 => 
     array (
@@ -352,44 +390,6 @@
     array (
       'id' => 'int',
       'category_id' => 'int',
-    ),
-    14 => 
-    array (
-    ),
-    19 => NULL,
-    20 => 
-    array (
-      'id' => 2,
-    ),
-  ),
-  'option' => 
-  array (
-    1 => 'App\\Domain\\Entity\\Option',
-    2 => 'Cycle\\ORM\\Mapper\\Mapper',
-    3 => 'Cycle\\ORM\\Select\\Source',
-    4 => 'Cycle\\ORM\\Select\\Repository',
-    5 => 'default',
-    6 => 'options',
-    7 => 
-    array (
-      0 => 'id',
-    ),
-    8 => 
-    array (
-      0 => 'id',
-    ),
-    9 => 
-    array (
-      'id' => 'id',
-      'name' => 'name',
-    ),
-    10 => 
-    array (
-    ),
-    12 => NULL,
-    13 => 
-    array (
-      'id' => 'int',
     ),
     14 => 
     array (
--- runtime/cache/listeners/fc30d4dfd70a6c5a9fd1d4c309e40210.php
+++ runtime/cache/listeners/fc30d4dfd70a6c5a9fd1d4c309e40210.php
@@ -1,10 +1,10 @@
 <?php return array (
-  0 => 'App\\Domain\\Entity\\ProductOption',
+  0 => 'App\\Domain\\Entity\\Option',
   1 => 'App\\Domain\\Entity\\ProductPrice',
-  2 => 'App\\Domain\\Entity\\OptionValue',
-  3 => 'App\\Domain\\Entity\\Category',
-  4 => 'App\\Domain\\Entity\\User',
-  5 => 'App\\Domain\\Entity\\Product',
-  6 => 'App\\Domain\\Entity\\Option',
+  2 => 'App\\Domain\\Entity\\ProductOption',
+  3 => 'App\\Domain\\Entity\\User',
+  4 => 'App\\Domain\\Entity\\Category',
+  5 => 'App\\Domain\\Entity\\OptionValue',
+  6 => 'App\\Domain\\Entity\\Product',
   7 => 'Spiral\\Cycle\\Auth\\Token',
 );
