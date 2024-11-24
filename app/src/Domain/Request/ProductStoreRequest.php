e4e48c2148c35b44441e7c8bd4dd6faad0e130a4
kaveh1999nazari <kaveh1999nazari@gmail.com>
2024-11-24 16:49:13 +0330

    fix function Rule

--- app/src/Domain/Request/ProductStoreRequest.php
+++ app/src/Domain/Request/ProductStoreRequest.php
@@ -4,10 +4,17 @@ namespace App\Domain\Request;
 
 
 use App\Domain\Entity\Option;
+use App\Domain\Entity\OptionValue;
 use Cycle\ORM\ORMInterface;
+use Cycle\ORM\RepositoryInterface;
+use function Symfony\Component\Translation\t;
 
 class ProductStoreRequest implements BaseRequest
 {
+    public function __construct(private readonly ORMInterface $orm)
+    {
+    }
+
     public function getRules(): array
     {
         return [
@@ -17,11 +24,20 @@ class ProductStoreRequest implements BaseRequest
             "categoryId" => ['required', 'integer'],
             "price" => ['required', 'string'],
             "options" => ['required', 'array', function ($values) {
-//                print_r($values);
-//                foreach ($values as $key => $value)
-//                {
-//                    print_r($key);
-//                }
+                foreach ($values as $key => $value) {
+                    $option = $this->orm->getRepository(Option::class)->findByPK($key);
+                    if (!$option) {
+                        return false;
+                    }
+                    foreach ($value as $v) {
+                        foreach ($v as $x) {
+                            $optionValue = $this->orm->getRepository(OptionValue::class)->findByPK($x);
+                            if (!$optionValue) {
+                                return false;
+                            }
+                        }
+                    }
+                }
                 return true;
             }],
         ];
