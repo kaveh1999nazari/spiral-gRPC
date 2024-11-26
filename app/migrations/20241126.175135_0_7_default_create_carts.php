10d1b9baff6bf0adeb59a2135fc0f27a8a061c2e
kaveh1999nazari <kaveh1999nazari@gmail.com>
2024-11-26 15:56:53 +0330

    add cart Table

--- app/migrations/20241126.175135_0_7_default_create_carts.php
+++ app/migrations/20241126.175135_0_7_default_create_carts.php
@@ -18,14 +18,22 @@ class OrmDefaultA42b7e366d78543ca8c5a4b60d305050 extends Migration
                 'unsigned' => false,
                 'zerofill' => false,
             ])
-            ->addColumn('user_id', 'integer', ['nullable' => true])
+            ->addColumn('user_id', 'integer')
             ->addForeignKey(['user_id'], 'users', ['id'], [
                 'onDelete' => 'CASCADE',
                 'onUpdate' => 'CASCADE'
             ])
-            ->addColumn('uuid', 'string', ['nullable' => true])
-            ->addColumn('price', 'string', ['nullable' => false, 'size' => 255])
+            ->addColumn('uuid', 'string')
+            ->addColumn('product_price_id', 'integer', ['nullable' => false])
+            ->addForeignKey(['product_price_id'], 'productprices', ['id'], [
+                'onDelete' => 'CASCADE',
+                'onUpdate' => 'CASCADE'
+            ])
+            ->addColumn('total_price', 'string', ['nullable' => false, 'size' => 255])
             ->setPrimaryKeys(['id'])
+            ->addIndex(['uuid', 'user_id'], [
+                'nullable' => true,
+            ])
             ->create();
     }
 
