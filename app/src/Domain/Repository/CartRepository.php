cf97a067ffa34ab12abd5c015f865bdba7f5352c
kaveh1999nazari <kaveh1999nazari@gmail.com>
2024-11-29 18:04:33 +0330

    Add Delete Cart By User

--- app/src/Domain/Repository/CartRepository.php
+++ app/src/Domain/Repository/CartRepository.php
@@ -6,13 +6,19 @@ use App\Domain\Entity\Cart;
 use App\Domain\Entity\ProductPrice;
 use App\Domain\Entity\User;
 use Cycle\ORM\EntityManager;
+use Cycle\ORM\ORMInterface;
 use Cycle\ORM\Select;
 use Cycle\ORM\Select\Repository;
+use Google\Rpc\Code;
+use Spiral\RoadRunner\GRPC\Exception\GRPCException;
 
 class CartRepository extends Repository
 {
 
-    public function __construct(Select $select, private readonly EntityManager $entityManager)
+    public function __construct(Select $select,
+                                private readonly EntityManager $entityManager,
+                                private readonly ORMInterface $ORM
+    )
     {
         parent::__construct($select);
     }
@@ -31,8 +37,25 @@ class CartRepository extends Repository
 
         return $cart;
 
-
-
     }
 
+    public function deleteByUser(int $userId): Cart
+    {
+        $carts = $this->ORM->getRepository(Cart::class)->select()->where('user_id', $userId)->fetchAll();
+
+        if(! $carts) {
+            throw new GRPCException(
+                message: "the cart is not exist!",
+                code: Code::UNAVAILABLE
+            );
+        }
+
+        foreach($carts as $cart) {
+            $this->entityManager->delete($cart);
+        }
+        $this->entityManager->run();
+
+        return $cart;
+
+    }
 }
