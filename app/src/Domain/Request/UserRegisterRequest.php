9ef0e949af9fad842c71c711a1e401a2b3e645eb
kaveh1999nazari <kaveh1999nazari@gmail.com>
2024-12-05 15:22:11 +0330

    update user Request

--- app/src/Domain/Request/UserRegisterRequest.php
+++ app/src/Domain/Request/UserRegisterRequest.php
@@ -7,13 +7,32 @@ class UserRegisterRequest implements BaseRequest
     public function getRules(): array
     {
         return [
+            "firstName" => [
+                ['string::longer', 2]
+            ],
+            "lastName" => [
+                ['string::longer', 2]
+            ],
             'mobile' => [
                 'required',
-                ['regexp', '/^0[0-9]{10}$/']
+                ['regexp', '/^0[0-9]{10}$/'],
+            ],
+            "email" => [
+                ['regexp', '/^[A-Za-z0-9._%+-]+@[A-Za-z.-]+\.[a-zA-Z]{2,}$/']
             ],
             'password' => [
                 'required',
-                ['string::length', 8]
+                ['string::longer', 5],
+                ['string::shorter', 10],
+            ],
+            "fatherName" => [
+                ['string::longer', 2]
+            ],
+            "nationalCode" => [
+                ['string::length', 10]
+            ],
+            "birthDate" => [
+                ['regexp', '/^\d{4}-\d{2}-\d{2}$/'],
             ]
         ];
     }
