0c78c2896dc548886e8c93358376a014838d005f
kaveh1999nazari <kaveh1999nazari@gmail.com>
2024-12-05 20:50:04 +0330

    add welcome notification

---
+++ app/views/emails/welcome.email.dark.php
@@ -0,0 +1,24 @@
+
+<extends:sendit:builder subject="Welcome to Spiral Shop!"/>
+<use:bundle path="sendit:bundle"/>
+
+<block:html>
+    <h1>Welcome, {{ $first_name }} {{ $last_name }}!</h1>
+    <p>
+        We're thrilled to have you join our team at Spiral Shop! 🎉
+    </p>
+    <p>
+        As a valued member, you can explore our wide range of products and enjoy shopping at the best prices available.
+        We are committed to providing you with the best experience and the most competitive offers.
+    </p>
+    <p>
+        Get started today and discover amazing deals tailored just for you!
+    </p>
+    <p>
+        If you have any questions, our support team is always here to help.
+    </p>
+    <p>
+        Happy Shopping! <br>
+        The Spiral gRPC Team
+    </p>
+</block:html>
