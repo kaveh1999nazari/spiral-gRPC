c808373ead6ee61df618e8f4ee76c425ee2d4b29
kaveh1999nazari <kaveh1999nazari@gmail.com>
2024-12-05 15:22:02 +0330

    update user Entity

--- app/src/Domain/Entity/User.php
+++ app/src/Domain/Entity/User.php
@@ -12,18 +12,63 @@ class User
     #[Column(type: 'primary')]
     private int $id;
     #[Column(type: 'string')]
+    private string $firstName;
+    #[Column(type: 'string')]
+    private string $lastName;
+    #[Column(type: 'string')]
+    private string $email;
+    #[Column(type: 'string')]
     private string $mobile;
     #[Column(type: 'string')]
     private string $password;
+    #[Column(type: 'string')]
+    private string $fatherName;
+    #[Column(type: 'string')]
+    private string $nationalCode;
+    #[Column(type: 'date')]
+    private \DateTimeImmutable $birthDate;
     #[Column(type: 'json')]
     private string $roles;
+    #[Column(type: 'datetime', default: 'now')]
+    private \DateTimeImmutable $created_at;
+    #[Column(type: 'datetime')]
+    private \DateTimeImmutable $updated_at;
 
     public function getId(): int
     {
         return $this->id;
     }
 
-    // Getter and Setter for Mobile
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): void
+    {
+        $this->firstName = $firstName;
+    }
+
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): void
+    {
+        $this->lastName = $lastName;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
     public function getMobile(): string
     {
         return $this->mobile;
@@ -45,6 +90,36 @@ class User
         $this->password = $password;
     }
 
+    public function getFatherName(): string
+    {
+        return $this->fatherName;
+    }
+
+    public function setFatherName(string $fatherName): void
+    {
+        $this->fatherName = $fatherName;
+    }
+
+    public function getNationalCode(): string
+    {
+        return $this->nationalCode;
+    }
+
+    public function setNationalCode(string $nationalCode): void
+    {
+        $this->nationalCode = $nationalCode;
+    }
+
+    public function getBirthDate(): \DateTimeImmutable
+    {
+        return $this->birthDate;
+    }
+
+    public function setBirthDate(\DateTimeImmutable $birthDate): void
+    {
+        $this->birthDate = $birthDate;
+    }
+
     public function getRoles(): array
     {
         return json_decode($this->roles, true);
@@ -53,6 +128,26 @@ class User
     public function setRoles(array $roles): void
     {
         $this->roles = json_encode($roles);
+    }
+
+    public function getCreatedAt(): \DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $create_at): void
+    {
+        $this->created_at = $create_at;
+    }
+
+    public function getUpdatedAt(): \DateTimeImmutable
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updated_at): void
+    {
+        $this->updated_at = $updated_at;
     }
 
 }
