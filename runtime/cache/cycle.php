52eaf24a99c1f85c5b0506b568820c023705ee00
kaveh1999nazari <kaveh1999nazari@gmail.com>
2024-12-05 10:42:58 +0330

    fix .rr.yaml with Docker server add comment that is standard manual yaml

--- runtime/cache/cycle.php
+++ runtime/cache/cycle.php
@@ -1,99 +1,4 @@
 <?php return array (
-  'option' => 
-  array (
-    1 => 'App\\Domain\\Entity\\Option',
-    2 => 'Cycle\\ORM\\Mapper\\Mapper',
-    3 => 'Cycle\\ORM\\Select\\Source',
-    4 => 'Cycle\\ORM\\Select\\Repository',
-    5 => 'default',
-    6 => 'options',
-    7 => 
-    array (
-      0 => 'id',
-    ),
-    8 => 
-    array (
-      0 => 'id',
-    ),
-    9 => 
-    array (
-      'id' => 'id',
-      'name' => 'name',
-    ),
-    10 => 
-    array (
-    ),
-    12 => NULL,
-    13 => 
-    array (
-      'id' => 'int',
-    ),
-    14 => 
-    array (
-    ),
-    19 => NULL,
-    20 => 
-    array (
-      'id' => 2,
-    ),
-  ),
-  'productPrice' => 
-  array (
-    1 => 'App\\Domain\\Entity\\ProductPrice',
-    2 => 'Cycle\\ORM\\Mapper\\Mapper',
-    3 => 'Cycle\\ORM\\Select\\Source',
-    4 => 'App\\Domain\\Repository\\ProductPriceRepository',
-    5 => 'default',
-    6 => 'productprices',
-    7 => 
-    array (
-      0 => 'id',
-    ),
-    8 => 
-    array (
-      0 => 'id',
-    ),
-    9 => 
-    array (
-      'id' => 'id',
-      'options' => 'options',
-      'price' => 'price',
-      'product_id' => 'product_id',
-    ),
-    10 => 
-    array (
-      'product' => 
-      array (
-        0 => 12,
-        1 => 'product',
-        3 => 10,
-        2 => 
-        array (
-          30 => true,
-          31 => false,
-          33 => 'product_id',
-          32 => 
-          array (
-            0 => 'id',
-          ),
-        ),
-      ),
-    ),
-    12 => NULL,
-    13 => 
-    array (
-      'id' => 'int',
-      'product_id' => 'int',
-    ),
-    14 => 
-    array (
-    ),
-    19 => NULL,
-    20 => 
-    array (
-      'id' => 2,
-    ),
-  ),
   'productOption' => 
   array (
     1 => 'App\\Domain\\Entity\\ProductOption',
@@ -185,14 +90,14 @@
       'id' => 2,
     ),
   ),
-  'user' => 
+  'productPrice' => 
   array (
-    1 => 'App\\Domain\\Entity\\User',
+    1 => 'App\\Domain\\Entity\\ProductPrice',
     2 => 'Cycle\\ORM\\Mapper\\Mapper',
     3 => 'Cycle\\ORM\\Select\\Source',
-    4 => 'App\\Domain\\Repository\\UserRepository',
+    4 => 'App\\Domain\\Repository\\ProductPriceRepository',
     5 => 'default',
-    6 => 'users',
+    6 => 'productprices',
     7 => 
     array (
       0 => 'id',
@@ -204,17 +109,90 @@
     9 => 
     array (
       'id' => 'id',
-      'mobile' => 'mobile',
-      'password' => 'password',
-      'roles' => 'roles',
+      'options' => 'options',
+      'price' => 'price',
+      'product_id' => 'product_id',
     ),
     10 => 
     array (
+      'product' => 
+      array (
+        0 => 12,
+        1 => 'product',
+        3 => 10,
+        2 => 
+        array (
+          30 => true,
+          31 => false,
+          33 => 'product_id',
+          32 => 
+          array (
+            0 => 'id',
+          ),
+        ),
+      ),
     ),
     12 => NULL,
     13 => 
     array (
       'id' => 'int',
+      'product_id' => 'int',
+    ),
+    14 => 
+    array (
+    ),
+    19 => NULL,
+    20 => 
+    array (
+      'id' => 2,
+    ),
+  ),
+  'optionValue' => 
+  array (
+    1 => 'App\\Domain\\Entity\\OptionValue',
+    2 => 'Cycle\\ORM\\Mapper\\Mapper',
+    3 => 'Cycle\\ORM\\Select\\Source',
+    4 => 'Cycle\\ORM\\Select\\Repository',
+    5 => 'default',
+    6 => 'optionvalues',
+    7 => 
+    array (
+      0 => 'id',
+    ),
+    8 => 
+    array (
+      0 => 'id',
+    ),
+    9 => 
+    array (
+      'id' => 'id',
+      'name' => 'name',
+      'option_id' => 'option_id',
+    ),
+    10 => 
+    array (
+      'option' => 
+      array (
+        0 => 12,
+        1 => 'option',
+        3 => 10,
+        2 => 
+        array (
+          30 => true,
+          31 => false,
+          33 => 'option_id',
+          32 => 
+          array (
+            0 => 'id',
+          ),
+        ),
+      ),
+    ),
+    12 => NULL,
+    13 => 
+    array (
+      'id' => 'int',
+      'option_id' => 'int',
     ),
     14 => 
     array (
@@ -271,6 +249,46 @@
           4 => NULL,
         ),
       ),
+    ),
+    12 => NULL,
+    13 => 
+    array (
+      'id' => 'int',
+    ),
+    14 => 
+    array (
+    ),
+    19 => NULL,
+    20 => 
+    array (
+      'id' => 2,
+    ),
+  ),
+  'user' => 
+  array (
+    1 => 'App\\Domain\\Entity\\User',
+    2 => 'Cycle\\ORM\\Mapper\\Mapper',
+    3 => 'Cycle\\ORM\\Select\\Source',
+    4 => 'App\\Domain\\Repository\\UserRepository',
+    5 => 'default',
+    6 => 'users',
+    7 => 
+    array (
+      0 => 'id',
+    ),
+    8 => 
+    array (
+      0 => 'id',
+    ),
+    9 => 
+    array (
+      'id' => 'id',
+      'mobile' => 'mobile',
+      'password' => 'password',
+      'roles' => 'roles',
+    ),
+    10 => 
+    array (
     ),
     12 => NULL,
     13 => 
@@ -363,62 +381,6 @@
       'id' => 2,
     ),
   ),
-  'optionValue' => 
-  array (
-    1 => 'App\\Domain\\Entity\\OptionValue',
-    2 => 'Cycle\\ORM\\Mapper\\Mapper',
-    3 => 'Cycle\\ORM\\Select\\Source',
-    4 => 'Cycle\\ORM\\Select\\Repository',
-    5 => 'default',
-    6 => 'optionvalues',
-    7 => 
-    array (
-      0 => 'id',
-    ),
-    8 => 
-    array (
-      0 => 'id',
-    ),
-    9 => 
-    array (
-      'id' => 'id',
-      'name' => 'name',
-      'option_id' => 'option_id',
-    ),
-    10 => 
-    array (
-      'option' => 
-      array (
-        0 => 12,
-        1 => 'option',
-        3 => 10,
-        2 => 
-        array (
-          30 => true,
-          31 => false,
-          33 => 'option_id',
-          32 => 
-          array (
-            0 => 'id',
-          ),
-        ),
-      ),
-    ),
-    12 => NULL,
-    13 => 
-    array (
-      'id' => 'int',
-      'option_id' => 'int',
-    ),
-    14 => 
-    array (
-    ),
-    19 => NULL,
-    20 => 
-    array (
-      'id' => 2,
-    ),
-  ),
   'product' => 
   array (
     1 => 'App\\Domain\\Entity\\Product',
@@ -467,6 +429,44 @@
     array (
       'id' => 'int',
       'category_id' => 'int',
+    ),
+    14 => 
+    array (
+    ),
+    19 => NULL,
+    20 => 
+    array (
+      'id' => 2,
+    ),
+  ),
+  'option' => 
+  array (
+    1 => 'App\\Domain\\Entity\\Option',
+    2 => 'Cycle\\ORM\\Mapper\\Mapper',
+    3 => 'Cycle\\ORM\\Select\\Source',
+    4 => 'Cycle\\ORM\\Select\\Repository',
+    5 => 'default',
+    6 => 'options',
+    7 => 
+    array (
+      0 => 'id',
+    ),
+    8 => 
+    array (
+      0 => 'id',
+    ),
+    9 => 
+    array (
+      'id' => 'id',
+      'name' => 'name',
+    ),
+    10 => 
+    array (
+    ),
+    12 => NULL,
+    13 => 
+    array (
+      'id' => 'int',
     ),
     14 => 
     array (
