c1565fab5ef63549965a9cb7f874c596a441dbed
kaveh1999nazari <kaveh1999nazari@gmail.com>
2024-12-06 16:53:13 +0330

    change text of otp.email.dark.php

--- app/views/emails/otp.email.dark.php
+++ app/views/emails/otp.email.dark.php
@@ -1,10 +1,10 @@
 
-<extends:sendit:builder subject="Your OTP Code"/>
+<extends:sendit:builder subject="Your Login Code"/>
 <use:bundle path="sendit:bundle"/>
 
 <block:html>
     <p>Hi {{ $name }}!</p>
-    <p>Your OTP code is: <strong>{{ $otp }}</strong></p>
+    <p>Your login code is: <strong>{{ $otp }}</strong></p>
     <p>This code is valid for 3 minutes.</p>
     <p>Best regards,<br>Spiral gRPC Team</p>
 </block:html>
