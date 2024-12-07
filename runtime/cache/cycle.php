4d00922e250898ec4026adace86fa6c935f8c2fd
kaveh1999nazari <kaveh1999nazari@gmail.com>
2024-12-07 14:33:20 +0330

    add UserJob migration

--- runtime/cache/cycle.php
+++ runtime/cache/cycle.php
@@ -172,6 +172,111 @@
       'id' => 2,
     ),
   ),
+  'userJob' => 
+  array (
+    1 => 'App\\Domain\\Entity\\UserJob',
+    2 => 'Cycle\\ORM\\Mapper\\Mapper',
+    3 => 'Cycle\\ORM\\Select\\Source',
+    4 => 'App\\Domain\\Repository\\UserJobRepository',
+    5 => 'default',
+    6 => 'user_jobs',
+    7 => 
+    array (
+      0 => 'id',
+    ),
+    8 => 
+    array (
+      0 => 'id',
+    ),
+    9 => 
+    array (
+      'id' => 'id',
+      'title' => 'title',
+      'phone' => 'phone',
+      'postalCode' => 'postal_code',
+      'address' => 'address',
+      'monthlySalary' => 'monthly_salary',
+      'workExperienceDuration' => 'work_experience_duration',
+      'workType' => 'work_type',
+      'contractType' => 'contract_type',
+      'createdAt' => 'created_at',
+      'updatedAt' => 'updated_at',
+      'user_id' => 'user_id',
+      'province_id' => 'province_id',
+      'city_id' => 'city_id',
+    ),
+    10 => 
+    array (
+      'user' => 
+      array (
+        0 => 12,
+        1 => 'user',
+        3 => 10,
+        2 => 
+        array (
+          30 => true,
+          31 => false,
+          33 => 'user_id',
+          32 => 
+          array (
+            0 => 'id',
+          ),
+        ),
+      ),
+      'province' => 
+      array (
+        0 => 12,
+        1 => 'province',
+        3 => 10,
+        2 => 
+        array (
+          30 => true,
+          31 => false,
+          33 => 'province_id',
+          32 => 
+          array (
+            0 => 'id',
+          ),
+        ),
+      ),
+      'city' => 
+      array (
+        0 => 12,
+        1 => 'city',
+        3 => 10,
+        2 => 
+        array (
+          30 => true,
+          31 => false,
+          33 => 'city_id',
+          32 => 
+          array (
+            0 => 'id',
+          ),
+        ),
+      ),
+    ),
+    12 => NULL,
+    13 => 
+    array (
+      'id' => 'int',
+      'monthlySalary' => 'float',
+      'workExperienceDuration' => 'int',
+      'createdAt' => 'datetime',
+      'updatedAt' => 'datetime',
+      'user_id' => 'int',
+      'province_id' => 'int',
+      'city_id' => 'int',
+    ),
+    14 => 
+    array (
+    ),
+    19 => NULL,
+    20 => 
+    array (
+      'id' => 2,
+    ),
+  ),
   'productOption' => 
   array (
     1 => 'App\\Domain\\Entity\\ProductOption',
