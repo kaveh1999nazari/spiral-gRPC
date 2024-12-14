41b8277c433794345be0f8f05ad9f98fcaa22774
kaveh1999nazari <kaveh1999nazari@gmail.com>
2024-12-14 16:13:27 +0330

    create order migration

---
+++ app/migrations/20241214.175143_0_15_default_create_orders.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace Migration;
+
+use Cycle\Migrations\Migration;
+
+class OrmDefaultA42b7e366d78543ca8c5a4b60d305057 extends Migration
+{
+    protected const DATABASE = 'default';
+
+    public function up(): void
+    {
+        $this->table('orders')
+            ->addColumn('id', 'primary', ['autoIncrement' => true])
+            ->addColumn('user_id', 'integer', ['nullable' => false])
+            ->addColumn('total_price', 'string', ['nullable' => true, 'size' => 255])
+            ->addColumn('status', 'enum', ['nullable' => true,
+                'values' =>['pending', 'place_order', 'accepted_order', 'send_product', 'received_product'] ])
+            ->addColumn('created_at', 'datetime', ['nullable' => true, 'default' => 'CURRENT_TIMESTAMP'])
+            ->addColumn('updated_at', 'datetime', ['nullable' => true, 'default' => 'CURRENT_TIMESTAMP'])
+            ->addForeignKey(['user_id'], 'users', ['id'], [
+                'onDelete' => 'CASCADE',
+                'onUpdate' => 'CASCADE'
+            ])
+            ->create();
+    }
+
+    public function down(): void
+    {
+        $this->table('orders')->drop();
+    }
+}
