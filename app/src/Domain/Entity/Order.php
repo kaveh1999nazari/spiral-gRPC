ef953ff074afb083dabe72681d2c12e52300b1b0
kaveh1999nazari <kaveh1999nazari@gmail.com>
2024-12-15 20:04:10 +0330

    add OrderItem in order Entity

--- app/src/Domain/Entity/Order.php
+++ app/src/Domain/Entity/Order.php
@@ -7,6 +7,7 @@ use App\Domain\Repository\OrderRepository;
 use Cycle\Annotated\Annotation\Column;
 use Cycle\Annotated\Annotation\Entity;
 use Cycle\Annotated\Annotation\Relation\BelongsTo;
+use Cycle\Annotated\Annotation\Relation\HasMany;
 
 #[Entity(repository: OrderRepository::class ,table: 'orders')]
 class Order
@@ -23,6 +24,8 @@ class Order
     private \DateTimeImmutable $createdAt;
     #[Column(type: 'datetime')]
     private \DateTimeImmutable $updatedAt;
+    #[HasMany(target: OrderItem::class)]
+    private array $orderItem;
 
     public function getId(): int
     {
@@ -77,6 +80,16 @@ class Order
     public function setUpdatedAt(\DateTimeImmutable $updatedAt): void
     {
         $this->updatedAt = $updatedAt;
+    }
+
+    public function getOrderItem(): array
+    {
+        return $this->orderItem;
+    }
+
+    public function setOrderItem(array $orderItem): void
+    {
+        $this->orderItem = $orderItem;
     }
 
 }
