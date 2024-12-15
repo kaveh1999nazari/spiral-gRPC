15216bd4c7004ffc849ce4c74cd391d8227acbf9
kaveh1999nazari <kaveh1999nazari@gmail.com>
2024-12-15 11:16:07 +0330

    add OrderItem update Repository

--- app/src/Domain/Repository/OrderItemRepository.php
+++ app/src/Domain/Repository/OrderItemRepository.php
@@ -6,12 +6,16 @@ use App\Domain\Entity\Order;
 use App\Domain\Entity\OrderItem;
 use App\Domain\Entity\User;
 use Cycle\ORM\EntityManager;
+use Cycle\ORM\ORMInterface;
 use Cycle\ORM\Select;
 use Cycle\ORM\Select\Repository;
 
 class OrderItemRepository extends Repository
 {
-    public function __construct(protected Select $select, private readonly EntityManager $entityManager)
+    public function __construct(protected Select $select,
+                                private readonly EntityManager $entityManager,
+                                private readonly ORMInterface $ORM
+    )
     {}
 
     public function create(User $user, Order $order,
@@ -28,6 +32,24 @@ class OrderItemRepository extends Repository
         $orderItem->setUpdatedAt(new \DateTimeImmutable());
 
         $this->entityManager->persist($orderItem);
+        $this->entityManager->run();
+
+        return $orderItem;
+    }
+
+    public function update(int $orderId, string $status): OrderItem
+    {
+        $orderItems = $this->ORM->getRepository(OrderItem::class)
+            ->select()
+            ->where(['order_id' => $orderId])
+            ->fetchAll();
+
+        foreach ($orderItems as $orderItem) {
+            $orderItem->setStatus($status);
+            $orderItem->setUpdatedAt(new \DateTimeImmutable());
+            $this->entityManager->persist($orderItem);
+        }
+
         $this->entityManager->run();
 
         return $orderItem;
