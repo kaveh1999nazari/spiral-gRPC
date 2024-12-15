915f21973ee511649a176d643278d932a4ec0817
kaveh1999nazari <kaveh1999nazari@gmail.com>
2024-12-15 11:16:15 +0330

    add Order update Repository

--- app/src/Domain/Repository/OrderRepository.php
+++ app/src/Domain/Repository/OrderRepository.php
@@ -5,12 +5,15 @@ namespace App\Domain\Repository;
 use App\Domain\Entity\Order;
 use App\Domain\Entity\User;
 use Cycle\ORM\EntityManager;
+use Cycle\ORM\ORMInterface;
 use Cycle\ORM\Select;
 use Cycle\ORM\Select\Repository;
 
 class OrderRepository extends Repository
 {
-    public function __construct(protected Select $select, private readonly EntityManager $entityManager)
+    public function __construct(protected Select $select,
+                                private readonly EntityManager $entityManager,
+                                private readonly ORMInterface $ORM)
     {}
 
     public function create(User $user, string $totalPrice): Order
@@ -20,6 +23,19 @@ class OrderRepository extends Repository
         $order->setTotalPrice($totalPrice);
         $order->setStatus('pending');
         $order->setCreatedAt(new \DateTimeImmutable());
+        $order->setUpdatedAt(new \DateTimeImmutable());
+
+        $this->entityManager->persist($order);
+        $this->entityManager->run();
+
+        return $order;
+    }
+
+    public function update(int $id, string $status): Order
+    {
+        $order = $this->ORM->getRepository(Order::class)->findByPK($id);
+
+        $order->setStatus($status);
         $order->setUpdatedAt(new \DateTimeImmutable());
 
         $this->entityManager->persist($order);
