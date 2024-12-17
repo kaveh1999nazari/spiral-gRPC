990609964dd9e0a6d8beb534f5905b848c4686bc
kaveh1999nazari <kaveh1999nazari@gmail.com>
2024-12-17 15:53:17 +0330

    change products migration

--- app/migrations/20241116.175132_0_2_default_create_products.php
+++ app/migrations/20241116.175132_0_2_default_create_products.php
@@ -12,16 +12,15 @@ class OrmDefaultA42b7e366d78543ca8c5a4b60d305045 extends Migration
     public function up(): void
     {
         $this->table('products')
-            ->addColumn('id', 'primary', [
-                'nullable' => false,
-                'autoIncrement' => true,
-                'unsigned' => false,
-                'zerofill' => false,
-            ])
-            ->addColumn('name', 'string', ['nullable' => false, 'defaultValue' => null, 'size' => 255])
-            ->addColumn('description', 'string', ['nullable' => true, 'size' => 500])
+            ->addColumn('id', 'primary', ['nullable' => false, 'autoIncrement' => true])
+            ->addColumn('name', 'string', ['nullable' => false, 'defaultValue' => null, 'size' => 150])
+            ->addColumn('brand', 'string', ['nullable' => true, 'size' => 150])
+            ->addColumn('description', 'text', ['nullable' => true])
             ->addColumn('image', 'json', ['nullable' => true])
+            ->addColumn('in_stock', 'enum', ['values' => ['yes', 'no']])
             ->addColumn('category_id', 'integer', ['nullable' => false])
+            ->addColumn('created_at', 'datetime', ['nullable' => true, 'default' => 'CURRENT_TIMESTAMP'])
+            ->addColumn('updated_at', 'datetime', ['nullable' => true, 'default' => 'CURRENT_TIMESTAMP'])
             ->addForeignKey(['category_id'], 'categories', ['id'], [
                 'onDelete' => 'CASCADE',
                 'onUpdate' => 'CASCADE'
