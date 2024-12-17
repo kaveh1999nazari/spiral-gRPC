47863f4a1e3a22863d9ed54cdc21256471efae58
kaveh1999nazari <kaveh1999nazari@gmail.com>
2024-12-17 17:09:07 +0330

    update ProductPrice Entity

--- app/src/Domain/Entity/ProductPrice.php
+++ app/src/Domain/Entity/ProductPrice.php
@@ -13,15 +13,16 @@ class ProductPrice
 {
     #[Column(type: "primary")]
     private int $id;
-
     #[BelongsTo(target: Product::class, nullable: false)]
     private Product $product;
-
     #[Column(type: "string")]
     private string $options;
-
     #[Column(type: "string")]
     private string $price;
+    #[Column(type: 'datetime')]
+    private \DateTimeImmutable $createdAt;
+    #[Column(type: 'datetime')]
+    private \DateTimeImmutable $updatedAt;
 
     public function getId(): int
     {
@@ -56,6 +57,26 @@ class ProductPrice
     public function setPrice(string $price): void
     {
         $this->price = $price;
+    }
+
+    public function getCreatedAt(): \DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    public function getUpdatedAt(): \DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
     }
 
 }
