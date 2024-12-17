374a73004e6efd4388a56f0d5da932f8d6b01ce9
kaveh1999nazari <kaveh1999nazari@gmail.com>
2024-12-17 15:53:37 +0330

    change productprices migration

--- app/migrations/20241120.175134_0_6_default_create_productprices.php
+++ app/migrations/20241120.175134_0_6_default_create_productprices.php
@@ -12,20 +12,17 @@ class OrmDefaultA42b7e366d78543ca8c5a4b60d305049 extends Migration
     public function up(): void
     {
         $this->table('productprices')
-            ->addColumn('id', 'primary', [
-                'nullable' => false,
-                'autoIncrement' => true,
-                'unsigned' => false,
-                'zerofill' => false,
-            ])
+            ->addColumn('id', 'primary', ['nullable' => false, 'autoIncrement' => true])
             ->addColumn('product_id', 'integer', ['nullable' => false])
+            ->addColumn('options', 'json', ['nullable' => false])
+            ->addColumn('price', 'string', ['nullable' => false, 'size' => 100])
+            ->addColumn('created_at', 'datetime', ['nullable' => true, 'default' => 'CURRENT_TIMESTAMP'])
+            ->addColumn('updated_at', 'datetime', ['nullable' => true, 'default' => 'CURRENT_TIMESTAMP'])
+            ->setPrimaryKeys(['id'])
             ->addForeignKey(['product_id'], 'products', ['id'], [
                 'onDelete' => 'CASCADE',
                 'onUpdate' => 'CASCADE'
             ])
-            ->addColumn('options', 'json', ['nullable' => false])
-            ->addColumn('price', 'string', ['nullable' => false, 'size' => 255])
-            ->setPrimaryKeys(['id'])
             ->create();
     }
 
