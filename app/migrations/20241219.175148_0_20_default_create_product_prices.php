3efad75734a3ffd71fb566230acb9a2f57cbd55b
kaveh1999nazari <kaveh1999nazari@gmail.com>
2024-12-19 21:59:58 +0330

    update product price migration

--- app/migrations/20241219.175148_0_20_default_create_product_prices.php
+++ app/migrations/20241219.175148_0_20_default_create_product_prices.php
@@ -5,17 +5,17 @@ namespace Migration;
 
 use Cycle\Migrations\Migration;
 
-class OrmDefaultA42b7e366d78543ca8c5a4b60d305049 extends Migration
+class OrmDefaultA42b7e366d78543ca8c5a4b60d305062 extends Migration
 {
     protected const DATABASE = 'default';
 
     public function up(): void
     {
-        $this->table('productprices')
+        $this->table('product_prices')
             ->addColumn('id', 'primary', ['nullable' => false, 'autoIncrement' => true])
             ->addColumn('product_id', 'integer', ['nullable' => false])
             ->addColumn('options', 'json', ['nullable' => false])
-            ->addColumn('price', 'string', ['nullable' => false, 'size' => 100])
+            ->addColumn('price', 'float', ['nullable' => false])
             ->addColumn('created_at', 'datetime', ['nullable' => true, 'default' => 'CURRENT_TIMESTAMP'])
             ->addColumn('updated_at', 'datetime', ['nullable' => true, 'default' => 'CURRENT_TIMESTAMP'])
             ->setPrimaryKeys(['id'])
@@ -28,6 +28,6 @@ class OrmDefaultA42b7e366d78543ca8c5a4b60d305049 extends Migration
 
     public function down(): void
     {
-        $this->table('productprices')->drop();
+        $this->table('product_prices')->drop();
     }
 }
