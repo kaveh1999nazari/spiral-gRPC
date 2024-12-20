64f299447bf3aa08feb2586722cb6180efffc922
kaveh1999nazari <kaveh1999nazari@gmail.com>
2024-12-20 10:07:03 +0330

    add product attribute Entity

--- app/src/Domain/Entity/ProductAttribute.php
+++ app/src/Domain/Entity/ProductAttribute.php
@@ -2,11 +2,12 @@
 
 namespace App\Domain\Entity;
 
+use App\Domain\Repository\ProductAttributeRepository;
 use Cycle\Annotated\Annotation\Column;
 use Cycle\Annotated\Annotation\Entity;
 use Cycle\Annotated\Annotation\Relation\BelongsTo;
 
-#[Entity(role: 'productAttribute', table: 'product_attributes')]
+#[Entity(repository: ProductAttributeRepository::class, table: 'product_attributes')]
 class ProductAttribute
 {
     #[Column(type: "primary")]
@@ -14,12 +15,14 @@ class ProductAttribute
 
     #[BelongsTo(target: Product::class, nullable: false)]
     private Product $product;
-
     #[BelongsTo(target: Attribute::class, nullable: false)]
     private Attribute $attribute;
-
     #[BelongsTo(target: AttributeValue::class, nullable: false)]
-    private AttributeValue $attributeValue;
+    private AttributeValue $attribute_value;
+    #[Column(type: 'datetime')]
+    private \DateTimeImmutable $createdAt;
+    #[Column(type: 'datetime')]
+    private \DateTimeImmutable $updatedAt;
 
     public function getId(): int
     {
@@ -48,12 +51,32 @@ class ProductAttribute
 
     public function getAttributeValue(): AttributeValue
     {
-        return $this->attributeValue;
+        return $this->attribute_value;
     }
 
-    public function setAttributeValue(AttributeValue $attributeValue): void
+    public function setAttributeValue(AttributeValue $attribute_value): void
     {
-        $this->attributeValue = $attributeValue;
+        $this->attribute_value = $attribute_value;
+    }
+
+    public function getCreatedAt(): \DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    public function getUpdatedAt(): \DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
     }
 
 }
