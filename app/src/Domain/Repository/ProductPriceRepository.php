288c77bfd3863b251369d364aeb962dfca109be9
kaveh1999nazari <kaveh1999nazari@gmail.com>
2024-12-23 08:45:47 +0330

    add discount in product price repository

--- app/src/Domain/Repository/ProductPriceRepository.php
+++ app/src/Domain/Repository/ProductPriceRepository.php
@@ -5,12 +5,14 @@ namespace App\Domain\Repository;
 use App\Domain\Entity\Product;
 use App\Domain\Entity\ProductPrice;
 use Cycle\ORM\EntityManager;
+use Cycle\ORM\ORMInterface;
 use Cycle\ORM\Select;
 use Cycle\ORM\Select\Repository;
 
 class ProductPriceRepository extends Repository
 {
-    public function __construct(Select $select, private readonly EntityManager $entityManager)
+    public function __construct(Select $select, private readonly EntityManager $entityManager,
+                                                private readonly ORMInterface $ORM)
     {
         parent::__construct($select);
     }
@@ -23,6 +25,24 @@ class ProductPriceRepository extends Repository
         $productPrice->setPrice($price);
         $productPrice->setCreatedAt(new \DateTimeImmutable());
         $productPrice->setUpdatedAt(new \DateTimeImmutable());
+
+        $this->entityManager->persist($productPrice);
+        $this->entityManager->run();
+
+        return $productPrice;
+    }
+
+    public function addDiscount(int $id, float $discountPercentage, string $time)
+    {
+        $productPrice = $this->ORM->getRepository(ProductPrice::class)
+            ->findByPK($id);
+
+        if (!$productPrice) {
+            throw new \Exception("ProductPrice with ID {$id} not found or can't be hydrated.");
+        }
+
+        $productPrice->setDiscountPercentage($discountPercentage);
+        $productPrice->setDiscountEndAt(new \DateTimeImmutable($time));
 
         $this->entityManager->persist($productPrice);
         $this->entityManager->run();
