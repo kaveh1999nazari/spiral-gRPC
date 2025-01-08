5c3353802ffe956b2cef360c4a9f048166436ab6
kaveh1999nazari <kaveh1999nazari@gmail.com>
2025-01-08 07:34:37 +0330

    create config for notifications

--- app/config/notifications.php
+++ app/config/notifications.php
@@ -1,6 +1,33 @@
 <?php
 
 return [
+    'channels' => [
+        'app' => [
+            'type' => 'app',
+            'class' => \App\Domain\Notifications\Channels\AppChannel::class,
+            'transport' => 'app_transport',
+        ],
+        'email' => [
+            'type' => 'email',
+            'transport' => 'smtp_transport',
+        ],
+    ],
+
+    'transports' => [
+        'app_transport' => 'null://',
+        'smtp_transport' => env('MAILER_DSN'),
+    ],
+
+    'policies' => [
+        'urgent' => ['app', 'email'],
+        'high' => ['app', 'email'],
+    ],
+
+    'typeAliases' => [
+        'email' => \App\Domain\Notifications\Channels\EmailChannel::class,
+        'app' => \App\Domain\Notifications\Channels\AppChannel::class,
+    ],
+
     'message' => [
         'login' => 'you have login successfully in :date: - :hour: !'
     ]
